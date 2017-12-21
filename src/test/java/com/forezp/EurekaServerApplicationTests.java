a6ed1b0cd710ec32f8ca1a5227fe3727890dbab1
Huliush <HuLiush@quarkfinance.com>
2017-12-21 19:54:16 +0800

    q

--- src/test/java/com/forezp/EurekaServerApplicationTests.java
+++ src/test/java/com/forezp/EurekaServerApplicationTests.java
@@ -12,7 +12,7 @@ public class EurekaServerApplicationTests {
 	@Test
 	public void contextLoads() {
 		System.out.println("1256");
-		System.out.println("4");
+		System.out.println("4111");
 		System.out.println("5");
 		System.out.println("6");
 	}
